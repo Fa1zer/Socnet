de1691d8d02d34480c25bf44fe37b63ab7f9cd84
WhyNotWhyYes <artemiy.zuzin@gmail.com>
2022-06-08 18:54:35 +0300

    initial

---
+++ VKApp/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  VKApp
+//
+//  Created by Artemiy Zuzin on 08.06.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UIViewController()
+        self.window?.makeKeyAndVisible()
+        self.window?.windowScene = windowScene
+    }
+    
+}
+
